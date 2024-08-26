abf0a584257ca68afc8f997db8a76f0d0f2ecb9d
Matt Crosby <mattcrosby01@gmail.com>
2024-08-26 16:08:49 -0400

    first commit idk what i am doing

---
+++ topic-01-simple-expressions/tokenizer.py
@@ -0,0 +1,5 @@
+#regular expressions
+    #simple form of a formal gramer that tell us exactly what we want to accept in a language
+    #as you get to more complicated expressions 
+    # example     [\d]+\.[d]*
+    # test
